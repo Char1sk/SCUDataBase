f47f32da986a30a455104603fb26c33a1d5b6add
Charisk <2553036255@qq.com>
2021-11-20 19:36:39 +0800

    finish extendible hash

--- MyScuDB/src/hash/extendible_hash.cpp
+++ MyScuDB/src/hash/extendible_hash.cpp
@@ -1,4 +1,5 @@
 #include <list>
+#include <iostream>
 
 #include "hash/extendible_hash.h"
 #include "page/page.h"
@@ -10,14 +11,24 @@ namespace scudb {
  * array_size: fixed array size for each bucket
  */
 template <typename K, typename V>
-ExtendibleHash<K, V>::ExtendibleHash(size_t size) {}
+ExtendibleHash<K, V>::ExtendibleHash(size_t size)
+     : globalDepth(0), bucketMaxSize(size), bucketNumbers(0)
+{
+    bucketTable.push_back(std::make_shared<Bucket>(0, size));
+}
 
 /*
  * helper function to calculate the hashing address of input key
+ * only use lower 'globalDepth' bits
  */
 template <typename K, typename V>
-size_t ExtendibleHash<K, V>::HashKey(const K &key) {
-  return 0;
+size_t ExtendibleHash<K, V>::HashKey(const K &key)
+{
+    // std::cout << getRealHashKey(key) << std::endl
+    //           << ((sizeof(size_t) << 3) - globalDepth) << std::endl
+    //           << (getRealHashKey(key) >> ((sizeof(size_t) << 3) - globalDepth)) << std::endl;
+    // return getRealHashKey(key) >> ((sizeof(size_t) << 3) - globalDepth);
+    return getRealHashKey(key) & ((1 << globalDepth) - 1);
 }
 
 /*
@@ -25,8 +36,9 @@ size_t ExtendibleHash<K, V>::HashKey(const K &key) {
  * NOTE: you must implement this function in order to pass test
  */
 template <typename K, typename V>
-int ExtendibleHash<K, V>::GetGlobalDepth() const {
-  return 0;
+int ExtendibleHash<K, V>::GetGlobalDepth() const
+{
+    return globalDepth;
 }
 
 /*
@@ -34,24 +46,37 @@ int ExtendibleHash<K, V>::GetGlobalDepth() const {
  * NOTE: you must implement this function in order to pass test
  */
 template <typename K, typename V>
-int ExtendibleHash<K, V>::GetLocalDepth(int bucket_id) const {
-  return 0;
+int ExtendibleHash<K, V>::GetLocalDepth(int bucket_id) const
+{
+    return bucketTable[bucket_id]->localDepth;
 }
 
 /*
  * helper function to return current number of bucket in hash table
  */
 template <typename K, typename V>
-int ExtendibleHash<K, V>::GetNumBuckets() const {
-  return 0;
+int ExtendibleHash<K, V>::GetNumBuckets() const
+{
+    return bucketNumbers;
 }
 
 /*
  * lookup function to find value associate with input key
  */
 template <typename K, typename V>
-bool ExtendibleHash<K, V>::Find(const K &key, V &value) {
-  return false;
+bool ExtendibleHash<K, V>::Find(const K &key, V &value)
+{
+    std::lock_guard<std::mutex> mguard(mutex);
+    
+    size_t bindex = HashKey(key);
+    auto pbucket = bucketTable[bindex];
+    // if pointer exists and bucket has this key
+    if (pbucket && pbucket->data.find(key) != pbucket->data.end())
+    {
+        value = pbucket->data[key];
+        return true;
+    }
+    return false;
 }
 
 /*
@@ -59,8 +84,19 @@ bool ExtendibleHash<K, V>::Find(const K &key, V &value) {
  * Shrink & Combination is not required for this project
  */
 template <typename K, typename V>
-bool ExtendibleHash<K, V>::Remove(const K &key) {
-  return false;
+bool ExtendibleHash<K, V>::Remove(const K &key)
+{
+    std::lock_guard<std::mutex> mguard(mutex);
+    
+    size_t bindex = HashKey(key);
+    std::shared_ptr<Bucket> pbucket = bucketTable[bindex];
+
+    if (!pbucket || pbucket->data.find(key) == pbucket->data.end())
+    {
+        return false;
+    }
+    pbucket->data.erase(key);
+    return true;
 }
 
 /*
@@ -69,7 +105,86 @@ bool ExtendibleHash<K, V>::Remove(const K &key) {
  * global depth
  */
 template <typename K, typename V>
-void ExtendibleHash<K, V>::Insert(const K &key, const V &value) {}
+void ExtendibleHash<K, V>::Insert(const K &key, const V &value)
+{
+    // std::cout << key << std::endl;
+    // std::cout << value << std::endl;
+    std::lock_guard<std::mutex> mguard(mutex);
+    
+    while (true)
+    {
+            
+        size_t bindex = 0;
+        if (globalDepth != 0)
+            bindex = HashKey(key);
+        std::shared_ptr<Bucket> pbucket = bucketTable[bindex];
+        // if the bucket has space, insert success
+        if (pbucket->data.size() < bucketMaxSize)
+        {
+            pbucket->data[key] = value;
+            break;
+        }
+        else
+        {
+            // split original bucket
+            // split it according to its new bit
+            std::shared_ptr<Bucket> zeroBucket = std::make_shared<Bucket>(pbucket->localDepth+1, bucketMaxSize);
+            std::shared_ptr<Bucket> oneBucket  = std::make_shared<Bucket>(pbucket->localDepth+1, bucketMaxSize);
+            for (auto &kvPair : pbucket->data)
+            {
+                // size_t newHashAddress = HashKey(kvPair.first);
+                size_t newRealHash = getRealHashKey(kvPair.first);
+                // if new bit is 1
+                if (newRealHash & (1 << pbucket->localDepth))
+                {
+                    oneBucket->data.insert(kvPair);
+                }
+                else
+                {
+                    zeroBucket->data.insert(kvPair);                        
+                }
+            }
+            // if the depth equals
+            // need to double the table
+            if (globalDepth == pbucket->localDepth)
+            {
+                ++globalDepth;
+                size_t osize = bucketTable.size();
+                for (size_t i = 0; i < osize; i++) {
+                    bucketTable.push_back(bucketTable[i]);
+                }
+                bucketNumbers *= 2;
+            }
+            // change items with the new bit
+            for (size_t i = 0; i < bucketTable.size(); i++)
+            {
+                if (bucketTable[i] == pbucket)
+                {
+                    if (i & (1 << pbucket->localDepth))
+                    {
+                        bucketTable[i] = oneBucket;
+                    }
+                    else
+                    {
+                        bucketTable[i] = zeroBucket;
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+/*
+ * get REAL hash value of the key
+ */
+template <typename K, typename V>
+size_t ExtendibleHash<K, V>::getRealHashKey(const K &key)
+{
+    // use std::hash, and then do hashing
+    return std::hash<K>()(key);
+}
+
 
 template class ExtendibleHash<page_id_t, Page *>;
 template class ExtendibleHash<Page *, std::list<Page *>::iterator>;
--- MyScuDB/src/include/hash/extendible_hash.h
+++ MyScuDB/src/include/hash/extendible_hash.h
@@ -12,6 +12,10 @@
 #include <cstdlib>
 #include <vector>
 #include <string>
+#include <map>
+#include <functional>
+#include <memory>
+#include <mutex>
 
 #include "hash/hash_table.h"
 
@@ -19,21 +23,54 @@ namespace scudb {
 
 template <typename K, typename V>
 class ExtendibleHash : public HashTable<K, V> {
+    // bucket used for hash
+    struct Bucket
+    {
+        // how many bits used to hash the bucket
+        int localDepth;
+        // maximum size of the bucket
+        size_t maxSize;
+        // place to store data in the bucket
+        // should store key and value to reallocate
+        // map is much more convenient than vector
+        std::map<K,V> data;
+        // construct a bucket with depth
+        Bucket(int depth = 0, size_t maxsize = 32)
+             : localDepth(depth), maxSize(maxsize) {}
+    };
+    
+    
 public:
-  // constructor
-  ExtendibleHash(size_t size);
-  // helper function to generate hash addressing
-  size_t HashKey(const K &key);
-  // helper function to get global & local depth
-  int GetGlobalDepth() const;
-  int GetLocalDepth(int bucket_id) const;
-  int GetNumBuckets() const;
-  // lookup and modifier
-  bool Find(const K &key, V &value) override;
-  bool Remove(const K &key) override;
-  void Insert(const K &key, const V &value) override;
+    // constructor
+    ExtendibleHash(size_t size);
+    // TODO:
+    // helper function to generate hash addressing
+    // @return: offset of the bucket
+    size_t HashKey(const K &key);
+    // TODO:
+    // helper function to get global & local depth
+    int GetGlobalDepth() const;
+    int GetLocalDepth(int bucket_id) const;
+    int GetNumBuckets() const;
+    // TODO:
+    // lookup and modifier
+    bool Find(const K &key, V &value) override;
+    bool Remove(const K &key) override;
+    void Insert(const K &key, const V &value) override;
+    // HELP:
+    // @return: the true hash value of key
+    size_t getRealHashKey(const K &key);
 
 private:
-  // add your own member variables here
+    // how many bits used for addressing
+    int globalDepth;
+    // maximum depth for buckets
+    size_t bucketMaxSize;
+    // how many buckets
+    size_t bucketNumbers;
+    // bucket index to bucket pointer
+    std::vector<std::shared_ptr<Bucket>> bucketTable;
+    // a mutex to be thread safe
+    std::mutex mutex;
 };
-} // namespace scudb
+}// namespace scudb
